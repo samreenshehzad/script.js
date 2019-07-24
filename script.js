e69baee2f265ad5b876b1bcbd8bfaafe0fcd36eb
Samreen Shehzad <49517251+samreenshehzad@users.noreply.github.com>
2019-07-24 17:54:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,34 @@
+//  function showAlert(){
+//     alert("hello world")
+// }
+let temperature = 35;
+if (temperature > 40) {
+    console.log("NotComing");
+}
+else {
+    console.log("coming");
+}
+let isDeadlineTomorrow = true;
+if (isDeadlineTomorrow == true) {
+    console.log("jagna hai");
+}
+else {
+    console.log("nahi jagna");
+}
+let distance = 0.5;
+if (distance < 1) {
+    console.log("walking");
+}
+
+else if (distance < 10) {
+    console.log("bus mai jao");
+}
+else {
+    console.log("careem");
+}
+for (let i = 0; i < 10; i++) {
+    console.log("clap!");
+}
+for (let i = 0; i < 10; i++) {
+    console.log(i);
+}
